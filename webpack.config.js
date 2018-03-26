fcdc493741833e439c794b67172d0357762e6c8d
λ • Geovani de Souza <geovanisouza92@gmail.com>
2018-03-26 13:52:22 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const slsw = require("serverless-webpack");
 
 module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: function() {
     return slsw.lib.entries;
   },
